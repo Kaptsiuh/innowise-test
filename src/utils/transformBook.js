fcf3c0cbe02a97aa382588c076fe58acc58650ac
Dima Berry <dimaberry1@gmail.com>
2026-04-29 14:27:21 +0300

    fix: add safe access for books without author_name field to prevent error

--- src/utils/transformBook.js
+++ src/utils/transformBook.js
@@ -1,7 +1,7 @@
 export function transformBook(book) {
   return {
     title: book.title,
-    authorName: book.author_name.at(0) || "Unknown author",
+    authorName: book?.author_name?.at(0) || "Unknown author",
     firstPublishYear: book.first_publish_year || "Unknown year",
     coverId: book.cover_i,
     key: book.key,
